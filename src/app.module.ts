aaaf42093a80ad76f029b5285ab77bc31b07d2f7
hkonate <konate.tronca@gmail.com>
2023-10-24 23:37:11 +0200

    validationPipe add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TontineModule } from './tontine/tontine.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule, UserModule, TontineModule, PrismaModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
